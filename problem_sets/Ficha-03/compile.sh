1c0a81edf604b30e597306ecc824a01c7017c746
eduardo106919 <a106919@alunos.uminho.pt>
2025-04-16 09:13:21 +0100

    clean up: shell scripts

--- problem_sets/Ficha-03/compile.sh
+++
@@ -1,7 +0,0 @@
-
-if [ -n "$1" ]; then
-    mkdir -p trash
-    javac -d trash $1.java
-else
-    echo "chose a class"
-fi
--- problem_sets/Ficha-03/run.sh
+++
@@ -1,7 +0,0 @@
-
-if [ -n "$1" ]; then
-    java -cp trash $1
-else
-    "chose a class"
-fi
-
