6a4bc3b4f44910a7d7cd21ceb139d19fad2114f7
eduardo106919 <a106919@alunos.uminho.pt>
2025-04-17 19:01:44 +0100

    update: class CasaInteligente

--- problem_sets/Ficha-04/CasaInteligente.java
+++ problem_sets/Ficha-04/CasaInteligente.java
@@ -1,10 +1,10 @@
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.Set;
-import java.util.Iterator;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 
 
 public class CasaInteligente {
@@ -21,25 +21,30 @@ public class CasaInteligente {
      */
 
     /**
-     * Construtor por omissão de uma Casa Inteligente
+     * Construtor por omissão de uma CasaInteligente
      */
     public CasaInteligente() {
         this.lampadas = new ArrayList<Lampada>();
     }
 
     /**
-     * Construtor de cópia de uma Casa Inteligente
+     * Construtor parametrizado de uma CasaInteligente
      *
-     * @param outro Casa Inteligente a copiar
+     * @param lampadas colecao de Lampadas
+     */
+    public CasaInteligente(Collection<Lampada> lampadas) {
+        this.lampadas = lampadas.stream().map(Lampada::clone).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    /**
+     * Construtor de cópia de uma CasaInteligente
+     *
+     * @param outro CasaInteligente a copiar
      */
     public CasaInteligente(CasaInteligente outro) {
-        this.lampadas = new ArrayList<Lampada>(outro.lampadas.size());
-        Iterator<Lampada> iterator = outro.lampadas.iterator();
-
-        while (iterator.hasNext()) {
-            this.lampadas.add(iterator.next().clone());
-        }
+        this.lampadas = outro.lampadas.stream().map(Lampada::clone).collect(Collectors.toCollection(ArrayList::new));
     }
+
 
     /**
      * Métodos de Instância
@@ -48,181 +53,135 @@ public class CasaInteligente {
     // getters
     
     /**
-     * Devolve as lampadas da casa
+     * Devolve as Lampadas da CasaInteligente
      *
      * @return lista de lampadas
      */
     public List<Lampada> get_lampadas() {
-        Iterator<Lampada> iterator = this.lampadas.iterator();
-        ArrayList<Lampada> result = new ArrayList<Lampada>(this.lampadas.size());
+        return this.lampadas.stream().map(Lampada::clone).collect(Collectors.toList());
+    }
 
-        while (iterator.hasNext()) {
-            result.add(iterator.next().clone());
-        }
+    // setters
+    
+    /**
+     * Altera as lampadas da CasaInteligente
+     *
+     * @param lampadas colecao de lampadas
+     */
+    public void set_lampadas(Collection<Lampada> lampadas) {
+        this.lampadas = lampadas.stream().map(Lampada::clone).collect(Collectors.toCollection(ArrayList::new));
+    }
 
-        return result;
+    // métodos de utilidade
+
+    /**
+     * Compara um objecto a um CasaInteligente
+     *
+     * @param outro objeto a comparar
+     * @return true se forem iguais
+     */
+    @Override
+    public boolean equals(Object outro) {
+        if (this == outro)
+            return true;
+        if ((outro == null) || (this.getClass() != outro.getClass()))
+            return false;
+        CasaInteligente temp = (CasaInteligente) outro;
+        return this.lampadas.equals(temp.lampadas);
+    }
+
+    /**
+     * Devolve uma representacao textual de uma CasaInteligente
+     *
+     * @return representacao textual
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        this.lampadas.forEach(l -> sb.append(l.toString()));
+
+        return sb.toString();
+    }
+
+    /**
+     * Cria uma cópia de uma CasaInteligente
+     *
+     * @return cópia de uma CasaInteligente
+     */
+    @Override
+    public CasaInteligente clone() {
+        return new CasaInteligente(this);
     }
 
     // outros métodos
-
+    
     /**
-     * Adiciona uma lampada à casa
+     * Adiciona uma lampada à CasaInteligente
      *
      * @param l lampada a adicionar
      */
-    public void adiciona_lampada(Lampada l) {
-        this.lampadas.add(l);
+    public void addLampada(Lampada l) {
+        this.lampadas.add(l.clone());
     }
 
-    /**
-     * Liga a lampada na posicao index, no modo normal
-     *
-     * @param index posicao da lampada a ligar
-     */
-    public void liga_lampada_normal(int index) {
+    public void ligaLampadaNormal(int index) {
         Lampada temp = this.lampadas.get(index);
         if (temp != null) {
             temp.lampON();
         }
     }
 
-    /**
-     * Liga a lampada na posicao index, no modo ECO
-     *
-     * @param index posicao da lampada a ligar
-     */
-    public void liga_lampada_eco(int index) {
+    public void ligaLampadaEco(int index) {
         Lampada temp = this.lampadas.get(index);
         if (temp != null) {
             temp.lampECO();
         }
     }
 
-    /**
-     * Compara dois objetos
-     *
-     * @param outro objeto a comparar
-     * @return true se forem iguais
-     */
-    public boolean equals(Object outro) {
-        if (outro == this)
-            return true;
-        if (outro == null || this.getClass() != outro.getClass())
-            return false;
-        CasaInteligente temp = (CasaInteligente) outro;
-        return this.lampadas.equals(temp.lampadas);
-    }
-    
-    /**
-     * Determina quantas lampadas estão no modo ECO
-     *
-     * @return número de lampadas
-     */
-    public int quantas_modo_ECO() {
-        return (int) this.lampadas.stream().filter(l -> l.getModo() == Lampada.Modo.ECO).count();
+    public int qtEmEco() {
+        return (int) this.lampadas.stream().filter(l -> l.getModo() == Modo.ECO).count();
     }
 
-
-    public void remove_lampada(int index) {
-        if (index < this.lampadas.size()) {
-            this.lampadas.remove(index);
-        }
+    public void removeLampada(int index) {
+        this.lampadas.remove(index);
     }
 
-    /**
-     * Liga todas as lampadas no modo ECO
-     */
-    public void liga_todas_ECO() {
-        Iterator<Lampada> iterator = this.lampadas.iterator();
-
-        while (iterator.hasNext()) {
-            iterator.next().lampECO();
-        }
+    public void ligaTodasEco() {
+        this.lampadas.forEach(Lampada::lampECO);
     }
 
-    /**
-     * Liga todas as lampadas no consumo
-     */
-    public void liga_todas_MAX() {
-        Iterator<Lampada> iterator = this.lampadas.iterator();
-
-        while (iterator.hasNext()) {
-            iterator.next().lampON();
-        }
+    public void ligaTodasMax() {
+        this.lampadas.forEach(Lampada::lampON);
     }
 
-    /**
-     * Determina o consumo total da casa
-     *
-     * @return consumo total
-     */
-    public double consumo_total() {
-        return this.lampadas.stream().mapToDouble(Lampada::getConsumoTotal).sum();
+    public double consumoTotal() {
+        return this.lampadas.stream().mapToDouble(Lampada::totalConsumo).sum();
     }
 
-    /**
-     * Determina a lampada com o maior consumo
-     *
-     * @return lampada com maior consumo, desde sempre
-     */
-    public Lampada mais_gastadora() {
-        Lampada max = null;
-        Iterator<Lampada> iterator = this.lampadas.iterator();
-        if (iterator.hasNext()) {
-            max = iterator.next();
-            Lampada temp = null;
-
-            while (iterator.hasNext()) {
-                temp = iterator.next();
-                if (temp.getConsumoTotal() > max.getConsumoTotal()) {
-                    max = temp;
-                }
-            }
-        }
-        
-        return max;
+    public Lampada maisGastadora() {
+        return this.lampadas.stream()
+                            .sorted((l1, l2) -> Double.compare(l2.totalConsumo(), l1.totalConsumo())) // inverter argumentos
+                            .findFirst()
+                            .orElse(null);
     }
 
-    /**
-     * Determina todas as lampadas que estão em modo ECO
-     *
-     * @return lampadas em modo ECO
-     */
-    public Set<Lampada> lampadas_ECO() {
-        return this.lampadas.stream().filter(l -> l.getModo() == Lampada.Modo.ECO).map(Lampada::clone).collect(Collectors.toSet());
+    public Set<Lampada> lampadasEmModoEco() {
+        return this.lampadas.stream()
+                            .filter(l -> l.getModo() == Modo.ECO)
+                            .map(Lampada::clone)
+                            .collect(Collectors.toSet());
     }
 
-    /**
-     * Efetua um reset do contador parcial a todas as lampadas
-     */
     public void reset() {
-        this.lampadas.forEach(l -> l.resetPeriodo());
+        this.lampadas.forEach(Lampada::resetPeriodo);
     }
 
-    /**
-     * Determina as 3 lampadas mais economicas
-     *
-     * @return 3 lampadas mais economicas
-     */
-    public Set<Lampada> podium_economia() {
-        return this.lampadas.stream().sorted((l1,l2) -> Double.compare(l1.getConsumoTotal(), l2.getConsumoTotal())).limit(3).collect(Collectors.toSet());
+    public Set<Lampada> podiumEconomia() {
+        return this.lampadas.stream()
+                            .sorted((l1, l2) -> Double.compare(l1.totalConsumo(), l2.totalConsumo()))
+                            .limit(3)
+                            .collect(Collectors.toSet());
     }
 
-    /**
-     * Clona uma casa inteligente
-     *
-     * @return cada clonada
-     */
-    public CasaInteligente clone() {
-        return new CasaInteligente(this);
-    }
-
-    /**
-     * Transforma uma casa inteligente numa string
-     *
-     * @return string com a informação das lampadas
-     */
-    public String toString() {
-        return this.lampadas.toString();
-    }
 }
