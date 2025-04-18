f282f4548d66b6b85292e9183027911b6382dba6
eduardo106919 <a106919@alunos.uminho.pt>
2025-04-18 15:17:01 +0100

    added: class PedidodeSuporte and SistemadeSuporte

--- problem_sets/Ficha-04/PedidodeSuporte.java
+++ problem_sets/Ficha-04/PedidodeSuporte.java
@@ -1,10 +1,8 @@
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 
-/**
- * Pedido de Suporte Informático
- */
 public class PedidodeSuporte {
 
     /**
@@ -12,12 +10,13 @@ public class PedidodeSuporte {
      */
 
     private String cliente;
-    private LocalDateTime instante_pedido;
+    private LocalDateTime data;
     private String assunto;
     private String descricao;
 
+    private boolean resolvido;
     private String funcionario;
-    private LocalDateTime instante_concluido;
+    private LocalDateTime conclusao;
     private String informacao;
 
 
@@ -26,50 +25,66 @@ public class PedidodeSuporte {
      */
 
     /**
-     * Constructor por omissão de um Pedido de Suporte Informático
+     * Construtor por omissão de um Pedido de Suporte
      */
     public PedidodeSuporte() {
-        this.cliente = "n/a";
-        this.instante_pedido = LocalDateTime.now();
-        this.assunto = "n/a";
-        this.descricao = "n/a";
-
-        this.funcionario = "n/a";
-        this.instante_concluido = null;
-        this.informacao = "n/a";
+        this.cliente = "";
+        this.data = LocalDateTime.now();
+        this.assunto = "";
+        this.descricao = "";
+        this.resolvido = false;
+        this.funcionario = "";
+        this.conclusao = LocalDateTime.now();
+        this.informacao = "";
     }
 
     /**
-     * Construtor parametrizado de um Pedido de Suporte Informático
+     * Construtor parametrizado de um Pedido de Suporte por terminar
      *
      * @param cliente nome do cliente
      * @param assunto assunto do pedido
      * @param descricao descricao do pedido
      */
     public PedidodeSuporte(String cliente, String assunto, String descricao) {
+        this();
         this.cliente = cliente;
-        this.instante_pedido = LocalDateTime.now();
         this.assunto = assunto;
         this.descricao = descricao;
-
-        this.funcionario = "n/a";
-        this.instante_concluido = null;
-        this.informacao = "n/a";
     }
 
     /**
-     * Construtor de cópia de um Pedido de Suporte Informático
+     * Construtor parametrizado de um Pedido de Suporte terminado
      *
-     * @param outro outro pedido de suporte
+     * @param cliente nome do cliente
+     * @param assunto assunto do pedido
+     * @param descricao descricao do pedido
+     * @param inicio data do pedido
+     * @param funcionario funcionario responsavel pelo pedido
+     * @param informacao informacao para o cliente
+     * @param fim data de resolucao do pedido
+     */
+    public PedidodeSuporte(String cliente, String assunto, String descricao, LocalDateTime inicio, String funcionario, String informacao, LocalDateTime fim) {
+        this(cliente, assunto, descricao);
+        this.resolvido = true;
+        this.data = inicio;
+        this.funcionario = funcionario;
+        this.informacao = informacao;
+        this.conclusao = fim;
+    }
+
+    /**
+     * Construtor de cópia para um Pedido de Suporte
+     *
+     * @param outro PedidodeSuporte a copiar
      */
     public PedidodeSuporte(PedidodeSuporte outro) {
         this.cliente = outro.cliente;
-        this.instante_pedido = outro.instante_pedido;
         this.assunto = outro.assunto;
         this.descricao = outro.descricao;
-
+        this.data = outro.data;
+        this.resolvido = outro.resolvido;
         this.funcionario = outro.funcionario;
-        this.instante_concluido = outro.instante_concluido;
+        this.conclusao = outro.conclusao;
         this.informacao = outro.informacao;
     }
 
@@ -77,25 +92,15 @@ public class PedidodeSuporte {
      * Métodos de Instância
      */
 
-
     // getters
-
+    
     /**
-     * Devolve o nome do cliente que efetuou o pedido
+     * Devolve o nome do cliente
      *
      * @return nome do cliente
      */
     public String get_cliente() {
-        return this.cliente;
-    }
-
-    /**
-     * Devolve o instante em que o pedido foi efetuado
-     *
-     * @return instante em que o pedido foi submetido
-     */
-    public LocalDateTime get_instante_pedido() {
-        return this.instante_pedido;
+        return cliente;
     }
 
     /**
@@ -104,59 +109,76 @@ public class PedidodeSuporte {
      * @return assunto do pedido
      */
     public String get_assunto() {
-        return this.assunto;
+        return assunto;
     }
 
     /**
-     * Devolve a descrição do pedido
+     * Devolve a descricao do pedido
      *
      * @return descricao do pedido
      */
     public String get_descricao() {
-        return this.descricao;
+        return descricao;
     }
 
     /**
-     * Devolve o nome do funcionário que tratou do pedido
+     * Devolve a data em que o pedido foi feito
      *
-     * @return nome do funcionário
+     * @return data do pedido
+     */
+    public LocalDateTime get_data() {
+        return data;
+    }
+
+    /**
+     * Devolve o nome do funcionario responsavel pelo pedido
+     *
+     * @return nome do funcionario
      */
     public String get_funcionario() {
-        return this.funcionario;
+        return funcionario;
     }
 
     /**
-     * Devolve o instante em que o pedido foi concluido
-     * Devolve null se ainda não terminou o pedido
+     * Devolve a informacao a transmitir ao cliente
      *
-     * @return instante em que o pedido foi concluido
-     */
-    public LocalDateTime get_instante_concluido() {
-        return this.instante_concluido;
-    }
-
-    /**
-     * Devolve a informação a ser transmitida ao cliente
-     *
-     * @return informação a ser transmitida ao cliente
+     * @return informacao a transmitir
      */
     public String get_informacao() {
-        return this.informacao;
+        return informacao;
+    }
+
+    /**
+     * Devolve a data de resolucao do pedido
+     *
+     * @return data de resolucao
+     */
+    public LocalDateTime get_data_resolucao() {
+        return conclusao;
+    }
+
+    /**
+     * Indica se o pedido está resolvido ou não
+     *
+     * @return true se estiver resolvido
+     */
+    public boolean get_resolvido() {
+        return resolvido;
     }
 
     // setters
     
     /**
-     * Altera o nome do cliente que efetuou o pedido
+     * Altera o nome do cliente
      *
-     * @param cliente nome do cliente
+     * @param nome nome do cliente
      */
     public void set_cliente(String cliente) {
         this.cliente = cliente;
     }
 
     /**
-     * Altera o assunto do pedido de suporte
+     * Altera o assunto do pedido
      *
      * @param assunto assunto do pedido
      */
@@ -165,84 +187,90 @@ public class PedidodeSuporte {
     }
 
     /**
-     * Altera a descricao do pedido de suporte
+     * Altera a descricao do pedido
      *
      * @param descricao descricao do pedido
      */
-    public void set_descrição(String descricao) {
+    public void set_descricao(String descricao) {
         this.descricao = descricao;
     }
 
+    // métodos de utilidade
+
     /**
-     * Atera o funcionário que trata do pedido
+     * Compara um objeto a um PedidodeSuporte
      *
-     * @param funcionario funcionario que trata do pedido
-     */
-    public void set_funcionario(String funcionario) {
-        this.funcionario = funcionario;
-    }
-
-    /**
-     * Coloca o pedido como terminado, se ainda nao terminou
-     */
-    public void set_instante_concluido() {
-        if (this.instante_concluido == null)
-            this.instante_concluido = LocalDateTime.now();
-    }
-
-    /**
-     * Altera a informação a transmitir ao cliente
-     *
-     * @param informacao informacao a transmitir
-     */
-    public void set_informacao(String informacao) {
-        this.informacao = informacao;
-    }
-
-    // outros métodos
-
-    /**
-     * Compara um objeto ao Pedido de Suporte chamador
-     *
-     * @param o objeto a compara
+     * @param outro objeto a comparar
      * @return true se forem iguais
      */
-    public boolean equals(Object o) {
-        if (o == this)
+    public boolean equals(Object outro) {
+        if (this == outro)
             return true;
-        if (o == null || this.getClass() != o.getClass())
+        if ((outro == null) || (this.getClass() != outro.getClass()))
             return false;
-        PedidodeSuporte temp = (PedidodeSuporte) o;
-        return this.cliente.equals(temp.cliente) && this.instante_pedido.equals(temp.instante_pedido)
-            && this.assunto.equals(temp.assunto) && this.descricao.equals(temp.descricao)
-            && this.funcionario.equals(temp.funcionario) && this.instante_concluido.equals(temp.instante_concluido)
-            && this.informacao.equals(temp.informacao);
+        PedidodeSuporte temp = (PedidodeSuporte) outro;
+        return this.cliente.equals(temp.cliente) && this.assunto.equals(temp.assunto)
+            && this.descricao.equals(temp.descricao) && this.data.equals(temp.data)
+            && this.resolvido == temp.resolvido && this.funcionario.equals(temp.funcionario)
+            && this.informacao.equals(temp.informacao) && this.conclusao.equals(temp.conclusao);
     }
 
     /**
-     * Clona um Pedido de Suporte
+     * Devolve uma representacao textual de um PedidodeSuporte
      *
-     * @return pedido de suporte clonado
+     * @return representacao textual
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Cliente: ").append(this.cliente);
+        sb.append("Assunto: ").append(this.assunto);
+        sb.append("Descricao: ").append(this.descricao);
+        sb.append("Data: ").append(this.data.toString());
+        if (this.resolvido == true) {
+            sb.append("Funcionario: ").append(this.funcionario);
+            sb.append("Informacao: ").append(this.informacao);
+            sb.append("Conclusao: ").append(this.conclusao);
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Cria uma cópia de um PedidodeSuporte
+     *
+     * @return cópia de um PedidodeSuporte
      */
     public PedidodeSuporte clone() {
         return new PedidodeSuporte(this);
     }
 
+    // outros métodos
+        
     /**
-     * Transforma um pedido de suporte numa string
+     * Resolve um PedidodeSuporte
      *
-     * @return string com a informacao de um pedido de suporte
+     * @param funcionario nome do funcionario
+     * @param informacao informacao a transmitir
      */
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("cliente: ").append(this.cliente);
-        sb.append("instante pedido: ").append(this.instante_pedido.toString());
-        sb.append("assunto: ").append(this.assunto);
-        sb.append("descricao: ").append(this.descricao);
-        sb.append("funcionario: ").append(this.funcionario);
-        sb.append("instante concluido: ").append(this.instante_concluido.toString());
-        sb.append("informacao: ").append(this.informacao);
+    public void resolver(String funcionario, String informacao) {
+        if (this.resolvido == false) {
+            this.resolvido = true;
+            this.funcionario = funcionario;
+            this.informacao = informacao;
+            this.conclusao = LocalDateTime.now();
+        }
+    }
 
-        return sb.toString();
+    /**
+     * Determina o tempo de resolucao de um Pedido de Suporte terminado
+     *
+     * @return tempo de resolucao
+     */
+    public long tempo_resolucao() {
+        if (this.resolvido) {
+            return Duration.between(this.data, this.conclusao).toMinutes();
+        }
+        return -1;
     }
 }
